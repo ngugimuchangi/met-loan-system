b1305e2ba6a0cbbfff1a89bb81306cf754266404
ngugimuchangi <tech.curious14@gmail.com>
2024-03-19 01:09:06 +0300

    Add dashboard layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
 
 export const routes: Routes = [
     {
         path: 'login',
         loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'dashboard',
+        canActivate: [authGuard],
+        loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent)
     }
+
 ];
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { User } from '../auth/user';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule,
+    MatIconModule, MatSidenavModule
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent implements OnInit {
+  user!: User;
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.user = this.authService.currentUser as User;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
+
