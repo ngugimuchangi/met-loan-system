ab74941a9af5d84f5c12b913a93ca1e3e4a1ea26
ngugimuchangi <tech.curious14@gmail.com>
2024-03-18 18:33:52 +0300

    Add login status checker functionality and comments for better documentation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,20 +7,36 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
-  private currentUser: User | null = null;
-  private users: User[] = users;
+  currentUser: User | null = null;
+  users: User[] = users;
 
   constructor(private router: Router) { }
 
+  /**
+   * Log the user in
+   * @param email - The email of the user
+   * @param password - The password of the user
+   */
   login(email: string, password: string) {
     const user = this.users.find((user) => user.email === email && user.password === password);
     if (!user) throw new Error('Invalid credentials');
     this.currentUser = user;
-    this.router.navigateByUrl('/home');
+    this.router.navigateByUrl('/dashboard');
   }
 
+  /**
+   * Log the user out
+   */
   logout() {
     this.currentUser = null;
     this.router.navigateByUrl('/login');
+  }
+
+  /**
+   * Check if the user is logged in
+   * @returns true if the user is logged in, false otherwise
+   */
+  isLoggedIn() {
+    return !!this.currentUser;
   }
 }
