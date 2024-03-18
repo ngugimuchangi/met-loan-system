2af5a7907da172539c18c20d53ddcb533fa761b0
ngugimuchangi <tech.curious14@gmail.com>
2024-03-18 18:35:30 +0300

    Define user model and provide sample user data

--- src/app/auth/user.ts
+++ src/app/auth/user.ts
@@ -6,4 +6,33 @@ export interface User {
     role: 'user' | 'admin'
 }
 
-export const users: User[] = [];
+export const users: User[] = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@gmail.com',
+        password: 'password',
+        role: 'user'
+    },
+    {
+        firstName: 'Alice',
+        lastName: 'Smith',
+        email: 'alicesmith@gmail.com',
+        password: 'password',
+        role: 'user'
+    },
+    {
+        firstName: 'Bob',
+        lastName: 'Johnson',
+        email: 'bobjohnson@gmail.com',
+        password: 'password',
+        role: 'admin'
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'janedoe@gmail.com',
+        password: 'password',
+        role: 'admin'
+    }
+];
