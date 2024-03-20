cf190aa2cf360279713d8e3701ed1ba07e1b08c6
ngugimuchangi <tech.curious14@gmail.com>
2024-03-20 12:49:22 +0300

    Add side bar responsiveness and link restriction by role
    Side bar changes will enable it to become response in mobile view.
    
    Navigation links are restricted by role to ensure user and admin can access different view

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -6,23 +6,40 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { User } from '../model/user';
 import { AuthService } from '../auth/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule,
+    NgIf, RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule,
     MatIconModule, MatSidenavModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   user!: User;
-  constructor(private authService: AuthService) { }
+  screenWidth: number = window.innerWidth;
+  screenWidthListener: any;
+
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
     this.user = this.authService.currentUser as User;
+    this.screenWidthListener = window.onresize = () => {
+      this.screenWidth = window.innerWidth;
+    };
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.screenWidthListener);
+    this.screenWidthListener = null;
+  }
+
+  get isAdmin() {
+    return this.authService.isAdmin();
   }
 
   logout(): void {
