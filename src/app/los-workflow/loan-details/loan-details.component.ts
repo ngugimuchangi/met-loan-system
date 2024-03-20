e13af381047b9b03df91c5bc565677b02bcbea27
ngugimuchangi <tech.curious14@gmail.com>
2024-03-20 12:52:21 +0300

    Add loan detail review feature

--- src/app/los-workflow/loan-details/loan-details.component.ts
+++ src/app/los-workflow/loan-details/loan-details.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CurrencyPipe, DatePipe, NgIf, TitleCasePipe } from '@angular/common';
+
+import { LoanService } from '../loan.service';
+import { Loan } from '../../model/loan';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-loan-details',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CurrencyPipe, DatePipe, TitleCasePipe, MatButtonModule, MatIconModule],
   templateUrl: './loan-details.component.html',
   styleUrl: './loan-details.component.scss'
 })
 export class LoanDetailsComponent {
+
+  loan: Loan = inject(MAT_DIALOG_DATA);
+  dialogRef: MatDialogRef<LoanDetailsComponent> = inject(MatDialogRef);
+
+  @Output() loanStatusChange = new EventEmitter<boolean>();
+
+
+  constructor(private loanService: LoanService, private authService: AuthService) { }
+
+  canChangeStatus() {
+    return this.authService.isAdmin() && this.loan.loanDetails.status === 'pending'
+  }
+  approveLoan() {
+    this.loanService.approveLoan(this.loan);
+    this.loanStatusChange.emit(true);
+    this.close();
+  }
+
+  rejectLoan() {
+    this.loanService.rejectLoan(this.loan);
+    this.loanStatusChange.emit(true);
+    this.close();
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
 
 }
--- src/app/los-workflow/loan-list/loan-list.component.ts
+++ src/app/los-workflow/loan-list/loan-list.component.ts
@@ -1,26 +1,29 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { DatePipe, NgFor, TitleCasePipe } from '@angular/common';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { CurrencyPipe, DatePipe, NgFor, TitleCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
+import { Subject, takeUntil } from 'rxjs';
 
 import { Loan, LoanStatus } from '../../model/loan';
+import { LoanDetailsComponent } from '../loan-details/loan-details.component';
 
 @Component({
   selector: 'app-loan-list',
   standalone: true,
   imports: [
-    NgFor, DatePipe, TitleCasePipe, MatButtonModule, MatTableModule, MatPaginatorModule,
-    MatIconModule, MatFormFieldModule, MatSelectModule],
+    NgFor, CurrencyPipe, DatePipe, TitleCasePipe, FormsModule, MatButtonModule, MatTableModule, MatPaginatorModule,
+    MatIconModule, MatFormFieldModule, MatSelectModule, MatDialogModule],
   templateUrl: './loan-list.component.html',
   styleUrl: './loan-list.component.scss'
 })
-export class LoanListComponent implements OnInit, AfterViewInit {
+export class LoanListComponent implements OnInit, AfterViewInit, OnDestroy {
   loans: Loan[] = [];
   loanStatus: LoanStatus[] = ['approved', 'pending', 'rejected'];
   dataSource!: MatTableDataSource<Loan>;
@@ -28,10 +31,13 @@ export class LoanListComponent implements OnInit, AfterViewInit {
     'id', 'applicantName', 'amount', 'status', 'applicationDate',
     'disbursementDate', 'dueDate', 'actions'
   ];
+  selectedStatus = '';
+
+  private destroy$ = new Subject<void>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(activateRoute: ActivatedRoute) {
+  constructor(activateRoute: ActivatedRoute, private dialogService: MatDialog) {
     this.loans = activateRoute.snapshot.data['loansList'];
   }
 
@@ -43,10 +49,30 @@ export class LoanListComponent implements OnInit, AfterViewInit {
     this.dataSource.paginator = this.paginator;
   }
 
-  applyFilter(filter: string): void {
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
+  applyFilter(filter: string): void {
     const loans = filter ? this.loans.filter(loan => loan.loanDetails.status.toLowerCase() === filter.toLowerCase()) : this.loans;
     this.dataSource = new MatTableDataSource(loans);
     this.dataSource.paginator = this.paginator;
+  }
+
+  viewLoanDetails(loan: Loan): void {
+    const dialogConfig: MatDialogConfig = {
+      width: 'auto',
+      maxHeight: '80vh',
+      panelClass: 'dialog',
+      data: loan
+    }
+    const dialog = this.dialogService.open(LoanDetailsComponent, dialogConfig);
+    const loanDetailsComponent = dialog.componentInstance;
+    const loanStatusChangeSub = loanDetailsComponent.loanStatusChange.pipe(takeUntil(this.destroy$)).subscribe(() => {
+      this.applyFilter(this.selectedStatus);
+      loanStatusChangeSub.unsubscribe();
+    }
+    );
   }
 }
